07973a53f8cd946d23299c8fe04aa21ed33625e2
RizqiMaulana14 <maulanafitrah14@gmail.com>
2026-04-10 15:24:53 +0700

    Menambah Clear Form

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -240,7 +240,13 @@ namespace CRUDMahasiswaADO
 
         private void ClearForm()
         {
-            
+            txtNIM.Clear();
+            txtNama.Clear();
+            cmbJK.SelectedIndex = -1;
+            txtAlamat.Clear();
+            txtKodeProdi.Clear();
+            dtpTanggalLahir.Value = DateTime.Now;
+            txtNIM.Focus();
         }
 
         private void Form1_Load(object sender, EventArgs e)
